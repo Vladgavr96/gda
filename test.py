420005b9c2f4aa95a8d13fff60246f6b42cdf364
Владимир Гаврилюк <gavrilyuk_v@garpix.com>
2023-07-11 20:59:56 +0300

    test branches

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print('Hello world')
+'''def test():
+    print('recursion')
+    test()
+
+test()'''
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(3))
+
+
+def make_a_dict(phone, **kwargs): # name, email, comment
+    pass
+
+make_a_dict(phone='88005553535')
+result = {
+    'phone': '88005553535',
+    #'name': 'admin',
+    #'comment': 'smth'
+}
