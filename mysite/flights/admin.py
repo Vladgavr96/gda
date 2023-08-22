3b02e745849cb17123ad181f7e3845261779e126
Владимир Гаврилюк <gavrilyuk_v@garpix.com>
2023-08-22 20:54:39 +0300

    html

--- mysite/flights/admin.py
+++ mysite/flights/admin.py
@@ -3,4 +3,14 @@ from .models import Flight, Airport
 
 # Register your models here.
 admin.site.register(Flight)
-admin.site.register(Airport)
+
+
+class FlightInline(admin.TabularInline):
+    model = Flight
+    fk_name = 'origin'
+    extra = 10
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city')
+    inlines = [FlightInline, ]
--- mysite/flights/urls.py
+++ mysite/flights/urls.py
@@ -3,6 +3,6 @@ from .views import hello, airports, flight
 
 urlpatterns = [
     # path('hello', hello),
-    path('', airports),
+    path('', airports, name='airports'),
     path('<int:flight_id>', flight, name='flight')
 ]
