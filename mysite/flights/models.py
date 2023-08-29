0e94fac4a248c25ceea9183e38145d015c8c39c9
Владимир Гаврилюк <gavrilyuk_v@garpix.com>
2023-08-29 18:01:51 +0300

    html

--- mysite/flights/models.py
+++ mysite/flights/models.py
@@ -14,10 +14,18 @@ class Airport(models.Model):
 
 
 class Flight(models.Model):
-    origin = models.ForeignKey(Airport, verbose_name='Точка отправления', related_name='flight_origin', on_delete=models.CASCADE, null=True)
-    destination = models.ForeignKey(Airport, verbose_name='Точка прибытия', related_name='flight_destination', on_delete=models.CASCADE, null=True)
+    origin = models.ForeignKey(Airport, verbose_name='Точка отправления', related_name='flight_origin',
+                               on_delete=models.CASCADE, null=True)
+    destination = models.ForeignKey(Airport, verbose_name='Точка прибытия', related_name='flight_destination',
+                                    on_delete=models.CASCADE, null=True)
     duration = models.IntegerField(verbose_name='Длительность')
     created = models.DateTimeField(verbose_name='Время создания', default=now)
 
     def __str__(self):
         return f'Вылет из {self.origin} в {self.destination}. Длительность {self.duration} часов\n'
+
+
+class Passenger(models.Model):
+    name = models.CharField(verbose_name='Имя', max_length=60)
+    second_name = models.CharField(verbose_name='Фамилия', max_length=60)
+    flights = models.ManyToManyField(Flight, verbose_name='Вылеты', related_name='passanger_flights')
--- mysite/flights/views.py
+++ mysite/flights/views.py
@@ -17,21 +17,26 @@ def airports(request):
 
 
 def flight(request, flight_id):
-    #f = Flight.objects.get(pk=flight_id)
+    # f = Flight.objects.get(pk=flight_id)
     f = get_object_or_404(Flight, pk=flight_id)
     context = {
         'flight': f
     }
     return render(request, 'flight.html', context)
 
+
 def date_filter(request):
     start_date = request.GET.get('start_date')
-    print(request.GET)
+    filter_type = request.GET.get('filter_type')
     f = None
     if start_date:
-        f = Flight.objects.filter(created__gte = start_date)
+        if filter_type == 'gte':
+            f = Flight.objects.filter(created__gte=start_date)
+        elif filter_type == 'lte':
+            f = Flight.objects.filter(created__lte=start_date)
     context = {
         'flights': f,
-        'date': start_date
+        'date': start_date,
+        'filter_type':filter_type
     }
     return render(request, 'date_filter.html', context)
